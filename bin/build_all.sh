821f627ea7546b8d5d36e88bc261829da8dd6193
Ville Heikkilä <ville.heikkila@tuni.fi>
2024-03-08 09:47:55 +0200

    Add build script

---
+++ bin/build_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ../native
+cargo build --release
+cp target/release/onnx-native-test ../bin
+
+cd ../wasm/wasi-nn
+cargo build --release --target=wasm32-wasi
+cp target/wasm32-wasi/release/wasi-nn-onnx-test.wasm ../../bin
+
+cd ../wasmtime-test
+cargo build --release
+cp target/release/wasmtime-test ../bin
