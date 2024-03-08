8f2481aafc0678445a6a16d8703b3a9356c0437d
Ville Heikkilä <ville.heikkila@tuni.fi>
2024-03-08 09:49:32 +0200

    Add image pre-preparation

--- wasm/wasi-nn/src/main.rs
+++ wasm/wasi-nn/src/main.rs
@@ -1,10 +1,15 @@
-extern crate image2tensor;
+extern crate image;
+// extern crate image2tensor;
 extern crate wasi_nn;
+extern crate anyhow;
+extern crate ndarray;
 
+use anyhow::Error;
 use std::{cmp::Ordering, ops::RangeFrom};
-
-use image2tensor::{ColorOrder, TensorType};
-use wasi_nn::{Error, ExecutionTarget, Graph, GraphBuilder, GraphEncoding, GraphExecutionContext};
+use image::{imageops::FilterType, Pixel};
+use ndarray::s;
+// use image2tensor::{ColorOrder, TensorType};
+use wasi_nn::{ExecutionTarget, Graph, GraphBuilder, GraphEncoding, GraphExecutionContext};
 
 
 #[derive(Debug)]
@@ -22,7 +27,7 @@ pub enum ErrorType {
 }
 
 
-fn load_model(filename: &str) -> Result<Graph, Error> {
+fn load_model(filename: &str) -> Result<Graph, wasi_nn::Error> {
     GraphBuilder::new(
         GraphEncoding::Onnx,
         ExecutionTarget::CPU
@@ -30,16 +35,68 @@ fn load_model(filename: &str) -> Result<Graph, Error> {
 }
 
 
-fn load_image(path: &str, width: u32, height: u32, precision: TensorType, color_order: ColorOrder) -> Result<Vec<u8>, String> {
-    image2tensor::convert_image_to_tensor_bytes(path, width, height, precision, color_order)
+fn f32_vec_to_bytes(data: Vec<f32>) -> Vec<u8> {
+    let chunks: Vec<[u8; 4]> = data.into_iter().map(|f| f.to_le_bytes()).collect();
+    let mut result: Vec<u8> = Vec::new();
+
+    // TODO
+    // simplify this to potentially a single map.
+    for c in chunks {
+        for u in c.iter() {
+            result.push(*u);
+        }
+    }
+    result
+}
+
+pub fn image_to_tensor(
+    path: &str,
+    height: u32,
+    width: u32,
+) -> Result<Vec<u8>, Error> {
+    println!("trying to load image {:#?}", path);
+    let image = image::imageops::resize(
+        &image::open(path)?,
+        width,
+        height,
+        ::FilterType::Triangle,
+    );
+
+    println!("resized image: {:#?}", image.dimensions());
+
+    let mut array = ndarray::Array::from_shape_fn((1, 3, 224, 224), |(_, c, j, i)| {
+        let pixel = image.get_pixel(i as u32, j as u32);
+        let channels = pixel.channels();
+
+        // range [0, 255] -> range [0, 1]
+        (channels[c] as f32) / 255.0
+    });
+
+    // Normalize channels to mean=[0.485, 0.456, 0.406] and std=[0.229, 0.224, 0.225]
+    let mean = [0.485, 0.456, 0.406];
+    let std = [0.229, 0.224, 0.225];
+    for c in 0..3 {
+        let mut channel_array = array.slice_mut(s![0, c, .., ..]);
+        channel_array -= mean[c];
+        channel_array /= std[c];
+    }
+
+    Ok(f32_vec_to_bytes(array.as_slice().unwrap().to_vec()))
+}
+
+
+fn load_image(path: &str, width: u32, height: u32) -> Result<Vec<u8>, String> {
+// fn load_image(path: &str, width: u32, height: u32, precision: TensorType, color_order: ColorOrder) -> Result<Vec<u8>, String> {
+    // image2tensor::convert_image_to_tensor_bytes(path, width, height, precision, color_order)
+    image_to_tensor(path, width, height).map_err(|e| e.to_string())
 }
 
 
 fn get_result(model: &Graph, image_name: &str) -> Result<(f32, i32), ErrorType> {
     const MODEL_IMAGE_WIDTH: u32 = 224;
     const MODEL_IMAGE_HEIGHT: u32 = 224;
-    const MODEL_IMAGE_PRECISION: TensorType = TensorType::F32;
-    const MODEL_IMAGE_COLOR_ORDER: ColorOrder = ColorOrder::RGB;
+    // const MODEL_IMAGE_PRECISION: TensorType = TensorType::F32;
+    // const MODEL_IMAGE_COLOR_ORDER: ColorOrder = ColorOrder::RGB;
     const MODEL_INPUT_DIMENSIONS: [usize; 4] = [1, 3, MODEL_IMAGE_WIDTH as usize, MODEL_IMAGE_HEIGHT as usize];
 
     let mut context: GraphExecutionContext<'_> = match model.init_execution_context() {
@@ -48,7 +105,8 @@ fn get_result(model: &Graph, image_name: &str) -> Result<(f32, i32), ErrorType> 
     };
     println!("Execution context created.");
 
-    let image = match load_image(image_name, MODEL_IMAGE_WIDTH, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_PRECISION, MODEL_IMAGE_COLOR_ORDER) {
+    let image = match load_image(image_name, MODEL_IMAGE_WIDTH, MODEL_IMAGE_HEIGHT) {
+    // let image = match load_image(image_name, MODEL_IMAGE_WIDTH, MODEL_IMAGE_HEIGHT, MODEL_IMAGE_PRECISION, MODEL_IMAGE_COLOR_ORDER) {
         Ok(image) => image,
         Err(_) => return Err(ErrorType::ImageLoad),
     };
