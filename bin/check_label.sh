52638d4df0ae0346506f7384f2fc627be8edd7fb
Ville Heikkilä <ville.heikkila@tuni.fi>
2024-04-14 13:25:40 +0300

    Improve label check script

--- bin/check_label.sh
+++ bin/check_label.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 index=$1;
-label=$(cat models/labels.txt | grep "$index;" | cut -d';' -f2);
 
-echo "Index ${index} corresponds to label: ${label}";
+function echo_instructions {
+   echo "Usage: ./check_label.sh <index>";
+   echo "- <index> must be an integer between 1 and 1000 with no leading zeros";
+}
+
+if [[ ! "$index" =~ ^[1-9][0-9]*$ ]]
+then
+    echo_instructions;
+elif (( "$index" < 1 || "$index" > 1000 ))
+then
+    echo_instructions;
+else
+    label=$(cat models/labels.txt | grep --max-count=1 "$index;" | cut -d';' -f2);
+    echo "Index ${index} corresponds to label: ${label}";
+fi
